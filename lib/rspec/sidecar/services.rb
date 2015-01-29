12d8dc7e1521a6fd05e253dd2594119af66c1d5f
Luke Amdor <luke.amdor@banno.com>
2015-01-28 20:59:17 -0600

    adding a set number of retries for service

--- lib/rspec/sidecar/services.rb
+++ lib/rspec/sidecar/services.rb
@@ -12,16 +12,22 @@ module RSpec::Sidecar::Services
 
   def service(name, type,
               zookeeper_host = ENV["ZOOKEEPER_PORT_2181_TCP_ADDR"] || "localhost",
-              zookeeper_port = ENV["ZOOKEEPER_PORT_2181_TCP_PORT"] || 2181)
+              zookeeper_port = ENV["ZOOKEEPER_PORT_2181_TCP_PORT"] || 2181,
+              retries = 5)
     ZK.open(zookeeper_host + ":" + zookeeper_port.to_s) do |zk|
       instances_path = "/banno/services/#{name}:#{type}"
       loop do
         instances = zk.children(instances_path)
-        unless instances.empty?
+        if instances.empty?
+          raise "service '#{name}:#{type}' not available" if retries == 0
+          retries -= 1
+          
+          puts "retrying to get instances for service '#{name}:#{type}' (tries left: #{retries})"
+          sleep 1
+        else
           instance_json = zk.get(instances_path + "/" + instances[0])[0]
           return JSON.parse(instance_json)
         end
-        sleep 1
       end
     end
   end
